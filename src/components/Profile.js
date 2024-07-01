8c4b70a66141aedc6006e70836fd767a0f910c6c
regulus55 <hakjoon55@gmail.com>
2024-07-01 21:55:28 +0900

    api로 정보 받아오기

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 const Profile = () => {
+    const [profileInfo, setProfileInfo] = useState({})
+
+    const getProfileInfo = async () => {
+        try{
+            const token = localStorage.getItem('token');
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            }
+            const result = await axios.get('http://localhost:7070/api/auth',config)
+            // ^^유알엘에 필요한 정보가 담겨있고, config 에 authorization 정보를 담겨있다
+            console.log('+++++++++++++++++++++',result.data.body)
+            setProfileInfo(result.data.body)
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    useEffect(()=>{
+        getProfileInfo()
+    },[])
+
     return (
         <div>
-            profile page 입니다
+            {}
         </div>
     );
 };
